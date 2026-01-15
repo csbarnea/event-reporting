564a7f2db07146ffcdfda0dae17ae6167bb8aee0
cristian.sbarnea <cristian.sbarnea@stud.acs.upb.ro>
2026-01-15 22:17:21 +0200

    Remove ip address of server for mobile application

--- mobile/lib/api.ts
+++ mobile/lib/api.ts
@@ -2,7 +2,7 @@ import Constants from "expo-constants";
 import type { Event, CreateIncidentRequest } from "./types";
 
 const API_BASE_URL =
-    Constants.expoConfig?.extra?.API_BASE_URL || "http://192.168.1.139:5000";
+    Constants.expoConfig?.extra?.API_BASE_URL || "SERVER IP";
 
 export async function fetchIncidents(): Promise<Event[]> {
     const response = await fetch(`${API_BASE_URL}/api/incidents`);
